233a59b2a6d315ed7bc7f264b2715520f55430bc
Srihari <srih4ri@gmail.com>
2011-09-09 19:06:51 +0530

    Change save location agin

--- pixelart.py
+++ pixelart.py
@@ -50,4 +50,4 @@ if __name__ == "__main__":
     pixel_size = [int(sys.argv[2]),int(sys.argv[3])]
     art = pixel_art(img,pixel_size)
     art.show()
-    art.save('pixelised_'+os.path.basename(img.filename))
+    art.save('art/pixelised_'+os.path.basename(img.filename))
